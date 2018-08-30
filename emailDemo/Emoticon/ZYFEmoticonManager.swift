fd3948cebe8e9803f499727f845e4197d38ec478
zhangyafeng123 <1240334911@qq.com>
2018-08-30 11:10:07 +0800

    加载表情包数据数组

--- emailDemo/Emoticon/ZYFEmoticonManager.swift
+++ emailDemo/Emoticon/ZYFEmoticonManager.swift
@@ -12,6 +12,8 @@ class ZYFEmoticonManager {
     //为了便于表情的复用，建立一个单例，只加载一次表情数据
     // 表情管理器的单例
     static let shared = ZYFEmoticonManager()
+    //表情包的懒加载数组
+    lazy var packages = [ZYFEmoticonPackage]()
     
     //构造函数，如果在 init 之前增加 private 修饰符，可以要求调用者必须通过 shared 访问对象
     private init () {
@@ -28,11 +30,16 @@ private extension ZYFEmoticonManager {
         guard let path = Bundle.main.path(forResource: "HMEmoticon.bundle", ofType: nil),
             let bundle = Bundle(path: path),
             let plistPath = bundle.path(forResource: "emoticons.plist", ofType: nil),
-            let array = NSArray(contentsOfFile: plistPath) as? [[String:String]] else {
+            let array = NSArray(contentsOfFile: plistPath) as? [[String:String]],
+            let models = NSArray.yy_modelArray(with: ZYFEmoticonPackage.self, json: array) as? [ZYFEmoticonPackage] else {
             return
         }
         
-        print(array)
+        //设置表情包数据
+        //使用 += 不需要再次给 packages 分配空间，直接追加数据
+        packages += models
+        
+        print(packages)
         
     }
 }
